dcffd7c328fb5fe4f33c394d1082787ef19f10f9
Jaychen01 <77700194+Jaychen01@users.noreply.github.com>
2021-02-21 03:24:07 -0500

    Q4 revise

--- Assignment4/Q4/IDString.java
+++ Assignment4/Q4/IDString.java
@@ -3,30 +3,26 @@ package Assignment4.Q4;
 public class IDString {
 	
 	public int[] idString(String S) {
-		int N = S.length(),t = 0;
+		int N = S.length();
+		int in = 0, de = N;
 		if(N < 1 || N >10000) {
 			return null;
 		}else {
 		int [] array = new int[N+1];
 		for(int i = 0; i < N; i++) {
 			if (S.charAt(i) == 'I') {
-				array[i] = t++;
-			}
+				array[i] = in++;
+			}else 
+				array[i] = de--;
 		}
-			array[N] = t++;
-			
-			for(int i = N-1; i > 0; i--) {
-				if (S.charAt(i) == 'D') {
-					array[i] = t++;
-				} 
-			}
+			array[N] = in;
 			return array;		
 		}
-	}
+}
 
 	public static void main(String[] args) {
 		
-		String S = "IDID";
+		String S = "DDD";
 		IDString str = new IDString();
 		int[] arr = str.idString(S);
 	
